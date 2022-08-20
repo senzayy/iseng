77b5dd9c8a6f738005b3f21608eed9fedbfa4a53
sendy <agustianosendy@gmail.com>
2022-08-20 11:46:32 +0700

    init

---
+++ src/main/java/com/zay/iseng/IsengApplication.java
@@ -0,0 +1,13 @@
+package com.zay.iseng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zay/iseng/IsengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zay.iseng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
